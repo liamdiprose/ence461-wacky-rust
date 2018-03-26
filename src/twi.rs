12dab7c633cf8f1c25861cb6095e4d0b8de46600
Liam Diprose <ldi30@uclive.ac.nz>
2018-03-26 18:52:28 +1300

    Added TWI

---
+++ src/twi.rs
@@ -0,0 +1,37 @@
+use mat91lib as mt91;
+
+struct Twi {
+    twi: mat91::twi_t
+}
+
+pub type Address = usize;
+
+impl Twi {
+    pub fn new (channel: u32, clock_speed: u16) -> Result<Self, ()> {
+        let mut cfg = twi_cfg {
+            channel,
+            period: clock_speed // TODO: TWI_PERIOD_DIVISOR
+        }
+
+        unsafe {
+            let me = Twi {
+                twi: twi_init(&cfg)
+            };
+            Ok(me)
+        }
+    }
+
+    pub fn write (slave: Address, iaddr: Address, buffer: &[u8]) {
+        mt91::twi_master_addr_write_timeout(self.twi, slave, iaddr, iaddr.len(), buffer, buffer.len())
+    }
+
+    pub fn read (slave: Address, iaddr: Address, buffer: &[u8]) {
+        mt91::twi_master_addr_read_timeout(self.twi, slave, buffer, buffer.len(), 100);  // TODO: Read timeout
+    }
+
+    // TODO: read timeout
+
+    pub fn shutdown(&self) {
+        mt91::twi_shutdown(self.twi)
+    }
+}
