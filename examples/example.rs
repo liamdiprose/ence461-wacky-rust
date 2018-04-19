ebf2e0446d66678c622d340081de4100da684670
liam <ldi30@uclive.ac.nz>
2018-04-19 17:58:18 +1200

    Added panic handler to example code

--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,20 @@
 #![no_std]
+#![feature(lang_items)]
+#![no_main]
+
 extern crate wacky;
 
-fn main () {
+use ::core::fmt::Arguments;
+
+#[no_mangle]
+pub fn main () {
     let buzzer = wacky::Pwm::new(1, 1, 1, true).unwrap();
     buzzer.start();
+}
+
+#[no_mangle]
+#[lang = "panic_fmt"]
+pub extern fn panic_fmt(_fmt: Arguments, _:&(&'static str, u32)) -> ! {
+    // TODO: Serial Error Message
+    loop {}
 }
