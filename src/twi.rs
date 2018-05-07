4de539975f3e5dc739b5ebf4d73c03c514c3e4ec
Liam Diprose <ldi30@uclive.ac.nz>
2018-05-07 19:28:45 +1200

    Added TWI

--- src/twi.rs
+++ src/twi.rs
@@ -6,7 +6,7 @@ pub struct Twi {
     twi: mt91::twi_t
 }
 
-pub type Address = usize;
+pub type Address = u32;
 
 impl Twi {
     pub fn new (channel: u32, clock_speed: u16) -> Result<Self, ()> {
@@ -25,31 +25,37 @@ impl Twi {
         }
     }
 
-    pub fn write (&self, slave: Address, iaddr: Address, buffer: &usize) {
+    pub fn write (&self, slave: Address, iaddr: Address, buffer: &mut [u8]) {
         unsafe {
             mt91::twi_master_addr_write_timeout(self.twi,
                                                 slave as u8,
                                                 iaddr as u32,
                                                 size_of_val(&iaddr) as u8,
-                                                buffer,
+                                                buffer.as_mut_ptr() as *mut usize,
                                                 size_of_val(buffer) as u16,
                                                 1000);
         }
     }
 
-    pub fn read (&self, slave: Address, iaddr: Address, buffer: &mut usize) {
+
+    pub fn read (&self, slave: Address, iaddr: Address, buffer: &mut [u8]) {
         unsafe {
             mt91::twi_master_addr_read_timeout(self.twi,
                                                slave as u8,
                                                iaddr as u32,
                                                size_of_val(&iaddr) as u8,
-                                               buffer,
+                                               buffer.as_mut_ptr() as *mut usize,
                                                size_of_val(buffer) as u16,
                                                1000);  // TODO: Read timeout
         }
     }
 
-    // TODO: read timeout
+    pub fn read_byte(&self, slave: Address, iaddr: Address) -> u8 {
+        let mut buffer = [0u8];  // Single byte
+        self.read(slave, iaddr, &mut buffer);
+
+        return buffer[0];
+    }
 
     pub fn shutdown(&self) {
         unsafe {
